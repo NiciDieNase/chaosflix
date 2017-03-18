c32fa08a627e8f471ba051b91ee50ea1d83a82cc
Felix <mail@nicidienase.de>
2017-03-18 19:30:04 +0100

    update entities

--- app/src/main/java/de/nicidienase/chaosflix/entities/Conferences.java
+++ app/src/main/java/de/nicidienase/chaosflix/entities/Conferences.java
@@ -1,14 +1,12 @@
 package de.nicidienase.chaosflix.entities;
 
-import com.orm.SugarRecord;
-
 import java.util.List;
 
 /**
  * Created by felix on 17.03.17.
  */
 
-public class Conferences extends SugarRecord{
+public class Conferences{
 	private List<Conference> conferences;
 
 	public List<Conference> getConferences() {
