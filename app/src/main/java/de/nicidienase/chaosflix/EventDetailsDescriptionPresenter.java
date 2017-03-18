c880be2e8065f770dae86047b345615c2a8f8840
Felix <mail@nicidienase.de>
2017-03-18 19:32:52 +0100

    replace Classes with my own implementations

---
+++ app/src/main/java/de/nicidienase/chaosflix/EventDetailsDescriptionPresenter.java
@@ -0,0 +1,33 @@
+package de.nicidienase.chaosflix;
+
+import android.support.v17.leanback.widget.AbstractDetailsDescriptionPresenter;
+import android.text.TextUtils;
+
+import de.nicidienase.chaosflix.entities.Event;
+
+/**
+ * Created by felix on 18.03.17.
+ */
+
+public class EventDetailsDescriptionPresenter extends AbstractDetailsDescriptionPresenter {
+	@Override
+	protected void onBindDescription(ViewHolder vh, Object item) {
+		Event event = (Event) item;
+
+		vh.getTitle().setText(event.getTitle());
+		vh.getSubtitle().setText(event.getSubtitle());
+		StringBuilder sb = new StringBuilder();
+		String speaker = TextUtils.join(", ", event.getPersons());
+		sb.append(event.getDescription())
+				.append("\n\n")
+				.append("by: ")
+				.append(speaker)
+				.append("\nreleased at: ")
+				.append(event.getReleaseDate())
+				.append("\nlast updated at: ")
+				.append(event.getUpdatedAt())
+				.append("\nTags: ")
+				.append(event.getTags());
+		vh.getBody().setText(sb.toString());
+	}
+}
--- app/src/main/java/de/nicidienase/chaosflix/fragments/ConferenceBrowseFragment.java
+++ app/src/main/java/de/nicidienase/chaosflix/fragments/ConferenceBrowseFragment.java
@@ -16,6 +16,7 @@ package de.nicidienase.chaosflix.fragments;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -44,10 +45,8 @@ import android.support.v4.app.ActivityOptionsCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
@@ -56,25 +55,22 @@ import com.bumptech.glide.request.target.SimpleTarget;
 
 import de.nicidienase.chaosflix.CardPresenter;
 import de.nicidienase.chaosflix.R;
-import de.nicidienase.chaosflix.activities.BrowseErrorActivity;
 import de.nicidienase.chaosflix.activities.DetailsActivity;
+import de.nicidienase.chaosflix.activities.ConferenceActivity;
+import de.nicidienase.chaosflix.activities.EventDetailsActivity;
 import de.nicidienase.chaosflix.entities.Conference;
 import de.nicidienase.chaosflix.entities.Event;
-import de.nicidienase.chaosflix.entities.Movie;
-import de.nicidienase.chaosflix.entities.MovieList;
 import de.nicidienase.chaosflix.network.MediaCCCClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainFragment extends BrowseFragment {
+public class ConferenceBrowseFragment extends BrowseFragment {
 	private static final String TAG = "MainFragment";
 
 	private static final int BACKGROUND_UPDATE_DELAY = 300;
 	private static final int GRID_ITEM_WIDTH = 200;
 	private static final int GRID_ITEM_HEIGHT = 200;
-	private static final int NUM_ROWS = 6;
-	private static final int NUM_COLS = 15;
 
 	private final Handler mHandler = new Handler();
 	private ArrayObjectAdapter mRowsAdapter;
@@ -83,18 +79,29 @@ public class MainFragment extends BrowseFragment {
 	private Timer mBackgroundTimer;
 	private URI mBackgroundURI;
 	private BackgroundManager mBackgroundManager;
+	private Conference conference;
+	private MediaCCCClient client = new MediaCCCClient();;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		Log.i(TAG, "onCreate");
 		super.onActivityCreated(savedInstanceState);
 
+		final int conferenceID = this.getActivity().getIntent().getIntExtra(ConferenceActivity.CONFERENCE_ID, 101);
+		client.getConference(conferenceID).enqueue(new Callback<Conference>() {
+			@Override
+			public void onResponse(Call<Conference> call, Response<Conference> response) {
+				conference = response.body();
+				setupUIElements();
+				loadRows();
+			}
+
+			@Override
+			public void onFailure(Call<Conference> call, Throwable t) {
+				// TODO handle failure
+			}
+		});
 		prepareBackgroundManager();
-
-		setupUIElements();
-
-		loadRows();
-
 		setupEventListeners();
 	}
 
@@ -108,47 +115,34 @@ public class MainFragment extends BrowseFragment {
 	}
 
 	private void loadRows() {
-		new MediaCCCClient().getConference(101).enqueue(new Callback<Conference>() {
-			@Override
-			public void onResponse(Call<Conference> call, Response<Conference> response) {
-				Conference conference = response.body();
-				HashMap<String, List<Event>> eventsByTags = conference.getEventsByTags();
+		HashMap<String, List<Event>> eventsByTags = conference.getEventsByTags();
 
-				mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
-				CardPresenter cardPresenter = new CardPresenter();
+		mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
+		CardPresenter cardPresenter = new CardPresenter();
 
-				List<Event> other = new LinkedList<Event>();
-				for (String tag: eventsByTags.keySet()) {
-					List<Event> items = eventsByTags.get(tag);
-					Collections.sort(items);
-					if(android.text.TextUtils.isDigitsOnly(tag)){
-						other.addAll(items);
-					} else {
-						ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(cardPresenter);
-						listRowAdapter.addAll(0, items);
-						HeaderItem header = new HeaderItem(tag);
-						mRowsAdapter.add(new ListRow(header, listRowAdapter));
-					}
-				}
-				if(other.size() > 0){
-					ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(cardPresenter);
-					listRowAdapter.addAll(0,other);
-					HeaderItem header = new HeaderItem("other");
-					mRowsAdapter.add(new ListRow(header,listRowAdapter));
-				}
-
-				setAdapter(mRowsAdapter);
+		List<Event> other = new LinkedList<Event>();
+		for (String tag: eventsByTags.keySet()) {
+			List<Event> items = eventsByTags.get(tag);
+			Collections.sort(items);
+			if(android.text.TextUtils.isDigitsOnly(tag)){
+				other.addAll(items);
+			} else {
+				ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(cardPresenter);
+				listRowAdapter.addAll(0, items);
+				HeaderItem header = new HeaderItem(tag);
+				mRowsAdapter.add(new ListRow(header, listRowAdapter));
 			}
-
-			@Override
-			public void onFailure(Call<Conference> call, Throwable t) {
-
-			}
-		});
+		}
+		if(other.size() > 0){
+			ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(cardPresenter);
+			listRowAdapter.addAll(0,other);
+			HeaderItem header = new HeaderItem("other");
+			mRowsAdapter.add(new ListRow(header,listRowAdapter));
+		}
+		setAdapter(mRowsAdapter);
 	}
 
 	private void prepareBackgroundManager() {
-
 		mBackgroundManager = BackgroundManager.getInstance(getActivity());
 		mBackgroundManager.attach(getActivity().getWindow());
 		mDefaultBackground = getResources().getDrawable(R.drawable.default_background);
@@ -157,15 +151,25 @@ public class MainFragment extends BrowseFragment {
 	}
 
 	private void setupUIElements() {
-		// setBadgeDrawable(getActivity().getResources().getDrawable(
-		// R.drawable.videos_by_google_banner));
-		setTitle(getString(R.string.browse_title)); // Badge, when set, takes precedent
+		Glide.with(getActivity())
+				.load(conference.getLogoUrl())
+				.centerCrop()
+				.error(mDefaultBackground)
+				.into(new SimpleTarget<GlideDrawable>(432, 243) {
+					@Override
+					public void onResourceReady(GlideDrawable resource,
+												GlideAnimation<? super GlideDrawable>
+														glideAnimation) {
+						setBadgeDrawable(resource);
+					}
+				});
+		setTitle(conference.getTitle()); // Badge, when set, takes precedent
 		// over title
 		setHeadersState(HEADERS_ENABLED);
 		setHeadersTransitionOnBackEnabled(true);
 
 		// set fastLane (or headers) background color
-		setBrandColor(getResources().getColor(R.color.black_opaque));
+		setBrandColor(getResources().getColor(R.color.fastlane_background));
 		// set search icon color
 		setSearchAffordanceColor(getResources().getColor(R.color.search_opaque));
 
@@ -202,18 +206,22 @@ public class MainFragment extends BrowseFragment {
 		mBackgroundTimer.schedule(new UpdateBackgroundTask(), BACKGROUND_UPDATE_DELAY);
 	}
 
+	public void setConference(Conference conference) {
+		this.conference = conference;
+	}
+
 	private final class ItemViewClickedListener implements OnItemViewClickedListener {
 		@Override
 		public void onItemClicked(Presenter.ViewHolder itemViewHolder, Object item,
 								  RowPresenter.ViewHolder rowViewHolder, Row row) {
 
 			Event event = (Event) item;
-			Intent i = new Intent(getActivity(), DetailsActivity.class);
-			i.putExtra(DetailsActivity.EVENT,event);
+			Intent i = new Intent(getActivity(), EventDetailsActivity.class);
+			i.putExtra(EventDetailsActivity.EVENT,event);
 			Bundle bundle = ActivityOptionsCompat.makeSceneTransitionAnimation(
 					getActivity(),
 					((ImageCardView) itemViewHolder.view).getMainImageView(),
-					DetailsActivity.SHARED_ELEMENT_NAME).toBundle();
+					EventDetailsActivity.SHARED_ELEMENT_NAME).toBundle();
 			getActivity().startActivity(i, bundle);
 		}
 	}
@@ -222,8 +230,13 @@ public class MainFragment extends BrowseFragment {
 		@Override
 		public void onItemSelected(Presenter.ViewHolder itemViewHolder, Object item,
 								   RowPresenter.ViewHolder rowViewHolder, Row row) {
-			if (item instanceof Movie) {
-				mBackgroundURI = ((Movie) item).getBackgroundImageURI();
+			if (item instanceof Event) {
+				try {
+					mBackgroundURI = new URI(((Event) item).getPosterUrl());
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				// TODO make configurable (enable/disable)
 				startBackgroundTimer();
 			}
 
@@ -245,28 +258,4 @@ public class MainFragment extends BrowseFragment {
 
 		}
 	}
-
-	private class GridItemPresenter extends Presenter {
-		@Override
-		public ViewHolder onCreateViewHolder(ViewGroup parent) {
-			TextView view = new TextView(parent.getContext());
-			view.setLayoutParams(new ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT));
-			view.setFocusable(true);
-			view.setFocusableInTouchMode(true);
-			view.setBackgroundColor(getResources().getColor(R.color.default_background));
-			view.setTextColor(Color.WHITE);
-			view.setGravity(Gravity.CENTER);
-			return new ViewHolder(view);
-		}
-
-		@Override
-		public void onBindViewHolder(ViewHolder viewHolder, Object item) {
-			((TextView) viewHolder.view).setText((String) item);
-		}
-
-		@Override
-		public void onUnbindViewHolder(ViewHolder viewHolder) {
-		}
-	}
-
 }
