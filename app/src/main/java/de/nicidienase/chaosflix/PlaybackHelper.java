f45f09056d19521ff141a4b15cda66ec8654dfb2
Felix <mail@nicidienase.de>
2017-03-26 23:13:31 +0200

    Event - change longs to ints

---
+++ app/src/main/java/de/nicidienase/chaosflix/PlaybackHelper.java
@@ -0,0 +1,10 @@
+package de.nicidienase.chaosflix;
+
+import android.support.v17.leanback.media.PlaybackControlGlue;
+
+/**
+ * Created by felix on 26.03.17.
+ */
+
+public class PlaybackHelper extends PlaybackControlGlue {
+}
