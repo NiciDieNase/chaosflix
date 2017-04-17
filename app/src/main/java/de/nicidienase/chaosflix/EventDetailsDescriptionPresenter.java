0b57489d728492fd721b9efb34f02ac2e74f3672
Felix <mail@nicidienase.de>
2017-04-17 14:02:37 +0200

    add new layout for details

--- app/src/main/java/de/nicidienase/chaosflix/EventDetailsDescriptionPresenter.java
+++ app/src/main/java/de/nicidienase/chaosflix/EventDetailsDescriptionPresenter.java
@@ -1,8 +1,15 @@
 package de.nicidienase.chaosflix;
 
+import android.content.Context;
 import android.support.v17.leanback.widget.AbstractDetailsDescriptionPresenter;
+import android.support.v17.leanback.widget.Presenter;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.ButterKnife;
 import de.nicidienase.chaosflix.entities.recording.Event;
 import de.nicidienase.chaosflix.entities.streaming.Room;
 
@@ -10,27 +17,48 @@ import de.nicidienase.chaosflix.entities.streaming.Room;
  * Created by felix on 18.03.17.
  */
 
-public class EventDetailsDescriptionPresenter extends AbstractDetailsDescriptionPresenter {
+public class EventDetailsDescriptionPresenter extends Presenter {
+
+	private final Context mContext;
+
+	public EventDetailsDescriptionPresenter(Context context){
+		mContext = context;
+	}
+
 	@Override
-	protected void onBindDescription(ViewHolder vh, Object item) {
+	public ViewHolder onCreateViewHolder(ViewGroup parent) {
+		View view = LayoutInflater.from(mContext).inflate(R.layout.detail_view_content, null);
+        return new ViewHolder(view);
+	}
+
+	@Override
+	public void onBindViewHolder(ViewHolder vh, Object item) {
+		TextView titleText = ButterKnife.findById(vh.view,R.id.title_text);
+		TextView speakersText = ButterKnife.findById(vh.view,R.id.speakers_text);
+		TextView subtitleText = ButterKnife.findById(vh.view,R.id.subtitle_text);
+		TextView descriptionText = ButterKnife.findById(vh.view,R.id.description_text);
 		if(item instanceof Event){
 			Event event = (Event) item;
 
-			vh.getTitle().setText(event.getTitle());
-			vh.getSubtitle().setText(event.getSubtitle());
-			StringBuilder sb = new StringBuilder();
+			titleText.setText(event.getTitle());
+			subtitleText.setText(event.getSubtitle());
 			String speaker = TextUtils.join(", ", event.getPersons());
+			speakersText.setText(speaker);
+			StringBuilder sb = new StringBuilder();
 			sb.append(event.getDescription())
 					.append("\n")
-					.append("by: ").append(speaker)
 					.append("\nreleased at: ").append(event.getReleaseDate())
-					.append("\nlast updated at: ").append(event.getUpdatedAt())
 					.append("\nTags: ").append(android.text.TextUtils.join(", ",event.getTags()));
-			vh.getBody().setText(sb.toString());
+			descriptionText.setText(sb.toString());
 		} else if(item instanceof Room){
 			Room room = (Room) item;
-			vh.getTitle().setText(room.getDisplay());
-			vh.getSubtitle().setText(room.getShedulename());
+			titleText.setText(room.getDisplay());
+			subtitleText.setText(room.getShedulename());
 		}
+	}
+
+	@Override
+	public void onUnbindViewHolder(ViewHolder viewHolder) {
+
 	}
 }
