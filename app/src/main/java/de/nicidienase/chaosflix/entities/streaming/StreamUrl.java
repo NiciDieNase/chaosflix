435aad5b057476691e9b282f3022788992273446
Felix <mail@nicidienase.de>
2017-04-21 20:58:45 +0200

    add different streams to dummy objects

--- app/src/main/java/de/nicidienase/chaosflix/entities/streaming/StreamUrl.java
+++ app/src/main/java/de/nicidienase/chaosflix/entities/streaming/StreamUrl.java
@@ -77,6 +77,33 @@ public class StreamUrl implements Parcelable {
 		dest.writeString(url);
 	}
 
+	public static StreamUrl getDummyObject(String codec){
+		StreamUrl dummy = getDummyObject();
+		switch (codec){
+			case "webm,vp8":
+				dummy.setUrl("https://storage.googleapis.com/wvmedia/clear/vp9/tears/tears.mpd");
+				dummy.setDisplay("webm,vp8");
+				break;
+			case "mp4,h265":
+				dummy.setUrl("https://storage.googleapis.com/wvmedia/clear/hevc/tears/tears_hd.mpd");
+				dummy.setDisplay("mp4,h265");
+				break;
+			case "hls":
+				dummy.setUrl("https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8");
+				dummy.setDisplay("hls");
+				break;
+			case "4x3":
+				dummy.setUrl("https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8");
+				dummy.setDisplay("4x3");
+				break;
+			case "dizzy":
+				dummy.setUrl("http://html5demos.com/assets/dizzy.mp4");
+				dummy.setDisplay("dizzy");
+				break;
+		}
+		return dummy;
+	}
+
 	public static StreamUrl getDummyObject(){
 		StreamUrl dummy = new StreamUrl();
 		dummy.setUrl("https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8");
