19e33947021abb10f163ff091c1df8707d29d8df
Felix <mail@nicidienase.de>
2017-09-14 12:04:33 +0200

    refactor packages to separate TV and phone/tablet code

---
+++ app/src/main/java/de/nicidienase/chaosflix/leanback/CardPresenter.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package de.nicidienase.chaosflix.leanback;
+
+import android.graphics.drawable.Drawable;
+import android.support.v17.leanback.widget.ImageCardView;
+import android.support.v17.leanback.widget.Presenter;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import de.nicidienase.chaosflix.R;
+import de.nicidienase.chaosflix.shared.entities.recording.Conference;
+import de.nicidienase.chaosflix.shared.entities.recording.Event;
+import de.nicidienase.chaosflix.shared.entities.streaming.LiveConference;
+import de.nicidienase.chaosflix.shared.entities.streaming.Room;
+
+/*
+ * A CardPresenter is used to generate Views and bind Objects to them on demand.
+ * It contains an Image CardView
+ */
+public class CardPresenter extends Presenter {
+	private static final String TAG = "CardPresenter";
+
+	private static final int CARD_WIDTH = 313;
+	private static final int CARD_HEIGHT_4 = 235;
+	private static final int CARD_HEIGHT_16 = 177;
+	private static int sSelectedBackgroundColor;
+	private static int sDefaultBackgroundColor;
+	private Drawable mDefaultCardImage;
+
+	private static void updateCardBackgroundColor(ImageCardView view, boolean selected) {
+		int color = selected ? sSelectedBackgroundColor : sDefaultBackgroundColor;
+		// Both background colors should be set because the view's background is temporarily visible
+		// during animations.
+		view.setBackgroundColor(color);
+		view.findViewById(R.id.info_field).setBackgroundColor(color);
+	}
+
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent) {
+		sDefaultBackgroundColor = parent.getResources().getColor(R.color.default_background);
+		sSelectedBackgroundColor = parent.getResources().getColor(R.color.selected_background);
+		mDefaultCardImage = parent.getResources().getDrawable(R.drawable.default_background);
+
+		ImageCardView cardView = new ImageCardView(parent.getContext()) {
+			@Override
+			public void setSelected(boolean selected) {
+				updateCardBackgroundColor(this, selected);
+				super.setSelected(selected);
+			}
+		};
+
+		cardView.setFocusable(true);
+		cardView.setFocusableInTouchMode(true);
+		updateCardBackgroundColor(cardView, false);
+		return new ViewHolder(cardView);
+	}
+
+	@Override
+	public void onBindViewHolder(Presenter.ViewHolder viewHolder, Object item) {
+		ImageCardView cardView = (ImageCardView) viewHolder.view;
+		cardView.setMainImageScaleType(ImageView.ScaleType.FIT_CENTER);
+		if (item instanceof Conference) {
+			cardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT_4);
+			Conference conference = (Conference) item;
+			cardView.setTitleText(conference.getTitle());
+			cardView.setContentText(conference.getAcronym());
+			if (conference.getLogoUrl() != null) {
+
+				Glide.with(viewHolder.view.getContext())
+						.load(conference.getLogoUrl())
+						.fitCenter()
+						.error(mDefaultCardImage)
+						.into(cardView.getMainImageView());
+			}
+		} else if (item instanceof Event) {
+			cardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT_16);
+			Event event = (Event) item;
+			cardView.setTitleText(event.getTitle());
+			cardView.setContentText(event.getSubtitle());
+//			cardView.setContentText(android.text.TextUtils.join(", ",event.getPersons()));
+			if (event.getThumbUrl() != null) {
+				Glide.with(viewHolder.view.getContext())
+						.load(event.getThumbUrl())
+						.fitCenter()
+						.error(mDefaultCardImage)
+						.into(cardView.getMainImageView());
+			}
+		} else if (item instanceof LiveConference) {
+			cardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT_4);
+			LiveConference con = (LiveConference) item;
+			cardView.setTitleText(con.getConference());
+			cardView.setMainImage(mDefaultCardImage);
+			cardView.setContentText(con.getDescription());
+		} else if (item instanceof Room) {
+			cardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT_16);
+			Room room = (Room) item;
+			cardView.setTitleText(room.getDisplay());
+			cardView.setContentText(room.getShedulename());
+			if (room.getThumb() != null) {
+				Glide.with(viewHolder.view.getContext())
+						.load(room.getThumb())
+						.fitCenter()
+						.error(mDefaultCardImage)
+						.into(cardView.getMainImageView());
+			}
+		} else if(item instanceof String) {
+			cardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT_4);
+			cardView.setTitleText((String) item);
+			Glide.with(viewHolder.view.getContext())
+					.load(R.drawable.icon)
+					.into(cardView.getMainImageView());
+		}
+
+	}
+
+	@Override
+	public void onUnbindViewHolder(Presenter.ViewHolder viewHolder) {
+		ImageCardView cardView = (ImageCardView) viewHolder.view;
+		// Remove references to images so that the garbage collector can free up memory
+		cardView.setBadgeImage(null);
+		cardView.setMainImage(null);
+	}
+}
---
+++ app/src/main/java/de/nicidienase/chaosflix/leanback/activities/EventsActivity.java
@@ -0,0 +1,20 @@
+package de.nicidienase.chaosflix.leanback.activities;
+
+import android.os.Bundle;
+
+import de.nicidienase.chaosflix.R;
+import de.nicidienase.chaosflix.shared.AbstractServiceConnectedActivity;
+
+public class EventsActivity extends AbstractServiceConnectedActivity {
+
+	public static final String CONFERENCE_ID = "conference_id";
+	public static final String CONFERENCE = "conference";
+	public static final String SHARED_ELEMENT_NAME = "shared_element";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO determin if we should use a Browse or a Grid layout
+		setContentView(R.layout.activity_events_browse);
+	}
+}
