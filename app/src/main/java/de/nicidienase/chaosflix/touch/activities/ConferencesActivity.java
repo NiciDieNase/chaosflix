704f42bb14c4b12758ed723f4fc50be8001c968b
Felix <mail@nicidienase.de>
2017-09-18 01:02:12 +0200

    start implementing touchUI

---
+++ app/src/main/java/de/nicidienase/chaosflix/touch/activities/ConferencesActivity.java
@@ -0,0 +1,11 @@
+package de.nicidienase.chaosflix.touch.activities;
+
+import de.nicidienase.chaosflix.shared.AbstractServiceConnectedActivity;
+
+/**
+ * Created by felix on 17.09.17.
+ */
+
+public class ConferencesActivity extends AbstractServiceConnectedActivity {
+
+}
