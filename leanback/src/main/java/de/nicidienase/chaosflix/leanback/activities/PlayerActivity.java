d4840f70df5d6abd7f35a03efadd6dc3e9eca897
Kai Renken <code@koffeinsucht.de>
2017-09-19 22:48:17 +0200

    set allowCrossProtocolRedirects true this fixes #14

--- leanback/src/main/java/de/nicidienase/chaosflix/leanback/activities/PlayerActivity.java
+++ leanback/src/main/java/de/nicidienase/chaosflix/leanback/activities/PlayerActivity.java
@@ -36,6 +36,7 @@ import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.HttpDataSource;
 import com.google.android.exoplayer2.util.Util;
@@ -321,6 +322,9 @@ public class PlayerActivity extends LeanbackBaseActivity
 	}
 
 	private HttpDataSource.Factory buildHttpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
-		return new DefaultHttpDataSourceFactory(mUserAgent, bandwidthMeter);
+		return new DefaultHttpDataSourceFactory(mUserAgent, bandwidthMeter,
+				DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS,
+				DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS,
+				true /* allowCrossProtocolRedirects */);
 	}
 }
