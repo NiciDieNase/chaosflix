e600987d65cd7e384994d63d85bda3fc2e636e1e
Felix <mail@nicidienase.de>
2017-09-20 23:55:13 +0200

    fix transformation animation, again

--- touch/src/main/java/de/nicidienase/chaosflix/touch/fragments/EventDetailsFragment.java
+++ touch/src/main/java/de/nicidienase/chaosflix/touch/fragments/EventDetailsFragment.java
@@ -139,7 +139,7 @@ public class EventDetailsFragment extends Fragment {
 				.asBitmap()
 				.fitCenter()
 				.dontAnimate()
-				.dontTransform()
+//				.dontTransform()
 //				.listener(new RequestListener<String, GlideDrawable>() {
 //					@Override
 //					public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
