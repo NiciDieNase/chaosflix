14cfeafa2238fa12147633ffe43e390d917ab212
Felix <mail@nicidienase.de>
2017-09-22 01:08:19 +0200

    start mediaplayerFragment and select MediaOption

--- touch/src/main/java/de/nicidienase/chaosflix/touch/fragments/MediaPlayerFragment.java
+++ touch/src/main/java/de/nicidienase/chaosflix/touch/fragments/MediaPlayerFragment.java
@@ -1,39 +1,47 @@
 package de.nicidienase.chaosflix.touch.fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.devbrackets.android.exomedia.ui.widget.VideoView;
 
+import java.util.Collections;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Optional;
 import de.nicidienase.chaosflix.R;
+import de.nicidienase.chaosflix.common.entities.PlayableItem;
 
 public class MediaPlayerFragment extends Fragment {
-	private static final String ARG_TITLE = "title";
-	private static final String ARG_URL = "url";
+	private static final String ARG_ITEM = "title";
+	private static final String ARG_INDEX = "index";
 
-	private String mTitle;
-	private String mVideoURL;
 	private OnMediaPlayerInteractionListener mListener;
 
 	@BindView(R.id.video_view)
 	VideoView videoView;
+	private PlayableItem mItem;
+	private int mIndex = -1;
 
 	public MediaPlayerFragment() {
 	}
 
-	public static MediaPlayerFragment newInstance(String title, String videoURL) {
+	public static MediaPlayerFragment newInstance(PlayableItem item, int index) {
 		MediaPlayerFragment fragment = new MediaPlayerFragment();
 		Bundle args = new Bundle();
-		args.putString(ARG_TITLE, title);
-		args.putString(ARG_URL,videoURL);
+		args.putParcelable(ARG_ITEM, item);
+		args.putInt(ARG_INDEX,index);
 		fragment.setArguments(args);
 		return fragment;
 	}
@@ -41,9 +49,10 @@ public class MediaPlayerFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
 		if (getArguments() != null) {
-			mTitle = getArguments().getString(ARG_TITLE);
-			mVideoURL = getArguments().getString(ARG_URL);
+			mItem = getArguments().getParcelable(ARG_ITEM);
+			mIndex = getArguments().getInt(ARG_INDEX);
 		}
 	}
 
@@ -60,8 +69,22 @@ public class MediaPlayerFragment extends Fragment {
 		ButterKnife.bind(this,view);
 
 		videoView.setOnPreparedListener(() -> videoView.start());
-		videoView.setVideoURI(Uri.parse(mVideoURL));
+		if(mIndex >= 0){
+			setVideoByIndex(mIndex);
+		} else {
+			List<String> strings = mItem.getPlaybackOptions();
+			CharSequence[] options = strings.toArray(new CharSequence[strings.size()]);
+			new AlertDialog.Builder(getActivity())
+					.setTitle(R.string.select_option)
+					.setItems(options,(dialog, which) -> setVideoByIndex(which))
+					.create().show();
+			}
+		}
+
+	private void setVideoByIndex(int index) {
+		videoView.setVideoURI(Uri.parse(mItem.getUrlForOption(index)));
 	}
+
 
 	@Override
 	public void onAttach(Context context) {
