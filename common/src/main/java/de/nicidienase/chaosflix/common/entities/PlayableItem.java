a2f316696496cbc9d829735f0859be4ff756debb
Felix <mail@nicidienase.de>
2017-09-22 01:08:19 +0200

    start mediaplayerFragment and select MediaOption

--- common/src/main/java/de/nicidienase/chaosflix/common/entities/PlayableItem.java
+++ common/src/main/java/de/nicidienase/chaosflix/common/entities/PlayableItem.java
@@ -1,12 +1,14 @@
 package de.nicidienase.chaosflix.common.entities;
 
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by felix on 22.09.17.
  */
 
-public interface PlayableItem {
+public interface PlayableItem extends Parcelable {
 	String getTitle();
 	String getSubtitle();
 	String getImageUrl();
