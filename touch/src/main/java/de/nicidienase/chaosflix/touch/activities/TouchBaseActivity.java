4dc7949116b9e8369b62759071c703f318af1dc9
Felix <mail@nicidienase.de>
2017-09-25 03:17:11 +0200

    cleanup unused and commented out code

--- touch/src/main/java/de/nicidienase/chaosflix/touch/activities/TouchBaseActivity.java
+++
@@ -1,9 +0,0 @@
-package de.nicidienase.chaosflix.touch.activities;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by felix on 24.03.17.
- */
-
-public class TouchBaseActivity extends AppCompatActivity {}
