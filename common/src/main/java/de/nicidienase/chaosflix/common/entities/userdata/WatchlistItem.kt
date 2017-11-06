ca67abea1e619a74c9df3c6266f5096a4419d5a7
Felix <mail@nicidienase.de>
2017-11-06 01:09:42 +0100

    add uniqueness-constraint to watchlist item

--- common/src/main/java/de/nicidienase/chaosflix/common/entities/userdata/WatchlistItem.kt
+++ common/src/main/java/de/nicidienase/chaosflix/common/entities/userdata/WatchlistItem.kt
@@ -1,13 +1,13 @@
 package de.nicidienase.chaosflix.common.entities.userdata
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Index
 import android.arch.persistence.room.PrimaryKey
 
-/**
- * Created by felix on 19.04.17.
- */
-
-@Entity(tableName = "watchlist_item")
+@Entity(tableName = "watchlist_item",
+        indices = arrayOf(Index(value = "event_id",unique = true)))
 data class WatchlistItem(@PrimaryKey
                          var id: Long,
+                         @ColumnInfo(name = "event_id")
                          var eventId: Long)
