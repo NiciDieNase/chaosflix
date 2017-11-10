f9dc39e64881b16a914546149daaef80d80ecf29
Felix <mail@nicidienase.de>
2017-11-10 17:16:16 +0100

    add splash-screen

---
+++ touch/src/main/java/de/nicidienase/chaosflix/touch/activities/SplashActivity.kt
@@ -0,0 +1,15 @@
+package de.nicidienase.chaosflix.touch.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this,BrowseActivity::class.java))
+        finish()
+    }
+}
