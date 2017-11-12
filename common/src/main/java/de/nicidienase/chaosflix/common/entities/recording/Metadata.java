c90dbb07f2f25cc9c6dd3b64001223bf8b084c1b
Felix <mail@nicidienase.de>
2017-11-12 17:09:14 +0100

    change related-ID list to map

--- common/src/main/java/de/nicidienase/chaosflix/common/entities/recording/Metadata.java
+++ common/src/main/java/de/nicidienase/chaosflix/common/entities/recording/Metadata.java
@@ -6,18 +6,25 @@ import android.os.Parcelable;
 import com.google.gson.annotations.SerializedName;
 import com.orm.SugarRecord;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by felix on 06.04.17.
  */
 
 public class Metadata extends SugarRecord implements Parcelable {
-	long[] related;
+	Map<Long,Long> related;
 	@SerializedName("remote_id")
 	String remoteId;
 
 	protected Metadata(Parcel in) {
-		related = in.createLongArray();
 		remoteId = in.readString();
+		long mapSize = in.readLong();
+		related = new HashMap<>();
+		for(int i= 0; i< mapSize; i++){
+			related.put(in.readLong(),in.readLong());
+		}
 	}
 
 	public static final Creator<Metadata> CREATOR = new Creator<Metadata>() {
@@ -32,11 +39,26 @@ public class Metadata extends SugarRecord implements Parcelable {
 		}
 	};
 
-	public long[] getRelated() {
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(remoteId);
+		dest.writeLong(related.size());
+		for(Map.Entry<Long,Long> entry: related.entrySet()){
+			dest.writeLong(entry.getKey());
+			dest.writeLong(entry.getValue());
+		}
+	}
+
+	public Map<Long, Long> getRelated() {
 		return related;
 	}
 
-	public void setRelated(long[] related) {
+	public void setRelated(Map<Long, Long> related) {
 		this.related = related;
 	}
 
@@ -46,16 +68,5 @@ public class Metadata extends SugarRecord implements Parcelable {
 
 	public void setRemoteId(String remoteId) {
 		this.remoteId = remoteId;
-	}
-
-	@Override
-	public int describeContents() {
-		return 0;
-	}
-
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeLongArray(related);
-		dest.writeString(remoteId);
 	}
 }
