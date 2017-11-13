f1fcfcfa09493913189349aba3ffbc22cb1b6e64
Felix <mail@nicidienase.de>
2017-11-13 23:32:06 +0100

    move Util to common

---
+++ common/src/main/java/de/nicidienase/chaosflix/common/Util.kt
@@ -0,0 +1,58 @@
+package de.nicidienase.chaosflix.touch
+
+import de.nicidienase.chaosflix.common.entities.recording.persistence.PersistentRecording
+import java.util.*
+import kotlin.collections.ArrayList
+
+object Util {
+    fun getOptimalStream(recordings: List<PersistentRecording>): PersistentRecording {
+        val result = ArrayList<PersistentRecording>()
+
+        result.addAll(recordings.filter { it.isHighQuality && it.mimeType == "video/mp4" }.sortedBy { it.language.length })
+        result.addAll(recordings.filter { !it.isHighQuality && it.mimeType == "video/mp4" }.sortedBy { it.language.length })
+        result.addAll(recordings.filter { it.mimeType.startsWith("video/") }.sortedBy { it.language.length })
+
+        when {
+            result.size > 0 -> return result[0]
+            else -> return recordings.get(0)
+        }
+    }
+
+    fun getStringForTag(tag: String): String {
+        when (tag) {
+            "congress" -> return "Congress"
+            "sendezentrum" -> return "Sendezentrum"
+            "camp" -> return "Camp"
+            "broadcast/chaosradio" -> return "Chaosradio"
+            "eh" -> return "Easterhegg"
+            "gpn" -> return "GPN"
+            "froscon" -> return "FrOSCon"
+            "mrmcd" -> return "MRMCD"
+            "sigint" -> return "SIGINT"
+            "datenspuren" -> return "Datenspuren"
+            "fiffkon" -> return "FifFKon"
+            "blinkenlights" -> return "Blinkenlights"
+            "chaoscologne" -> return "1c2 Chaos Cologne"
+            "cryptocon" -> return "CryptoCon"
+            "other conferences" -> return "Other Conferences"
+            "denog" -> return "DENOG"
+            "vcfb" -> return "Vintage Computing Festival Berlin"
+            "hackover" -> return "Hackover"
+            "netzpolitik" -> return "Das ist Netzpolitik!"
+            else -> return tag
+        }
+    }
+
+    val orderedConferencesList: List<String> = Arrays.asList("congress",
+            "gpn",
+            "mrmcd",
+            "broadcast/chaosradio",
+            "eh",
+            "camp",
+            "froscon",
+            "sendezentrum",
+            "sigint",
+            "datenspuren",
+            "fiffkon",
+            "cryptocon")
+}
