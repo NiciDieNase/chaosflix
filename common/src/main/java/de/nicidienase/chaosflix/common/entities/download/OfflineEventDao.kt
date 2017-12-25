7862c03b6e8baeff0f73b2acee30650dc949c0ff
Felix <mail@nicidienase.de>
2017-12-25 01:57:59 +0100

    add a few syncronous querys for offlineItem

--- common/src/main/java/de/nicidienase/chaosflix/common/entities/download/OfflineEventDao.kt
+++ common/src/main/java/de/nicidienase/chaosflix/common/entities/download/OfflineEventDao.kt
@@ -14,11 +14,21 @@ interface OfflineEventDao{
     @Query("SELECT * FROM offline_event WHERE event_id = :id LIMIT 1")
     fun getByEventId(id: Long): LiveData<OfflineEvent>
 
+    @Query("SELECT * FROM offline_event WHERE event_id = :id LIMIT 1")
+    fun getByEventIdSynchronous(id: Long): OfflineEvent
+
     @Query("SELECT * FROM offline_event WHERE download_reference = :ref LIMIT 1")
     fun getByDownloadReference(ref: Long): LiveData<OfflineEvent>
 
+    @Query("SELECT * FROM offline_event WHERE download_reference = :ref LIMIT 1")
+    fun getByDownloadReferenceSyncrounous(ref: Long): OfflineEvent
+
     @Query("SELECT * FROM offline_event")
     fun getAll(): LiveData<List<OfflineEvent>>
+
+    @Query("SELECT * FROM offline_event")
+    fun getAllSynchronous(): List<OfflineEvent>
+
 
     @Query("DELETE FROM offline_event WHERE id=:id")
     fun deleteById(id: Long)
