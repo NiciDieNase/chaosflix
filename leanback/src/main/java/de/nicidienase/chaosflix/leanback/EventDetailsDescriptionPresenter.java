f7835774ddedc664ea5f6b6ed11f7f4f66c6ddb3
Felix <mail@nicidienase.de>
2018-09-11 21:20:31 +0200

    Leanback: fix EventDetailsDescriptionPresenter

--- leanback/src/main/java/de/nicidienase/chaosflix/leanback/EventDetailsDescriptionPresenter.java
+++ leanback/src/main/java/de/nicidienase/chaosflix/leanback/EventDetailsDescriptionPresenter.java
@@ -3,6 +3,7 @@ package de.nicidienase.chaosflix.leanback;
 import android.content.Context;
 import android.support.v17.leanback.widget.Presenter;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import de.nicidienase.chaosflix.R;
 import de.nicidienase.chaosflix.common.mediadata.entities.recording.persistence.PersistentEvent;
 import de.nicidienase.chaosflix.common.mediadata.entities.streaming.Room;
+import de.nicidienase.chaosflix.databinding.DetailViewBinding;
 
 /**
  * Created by felix on 18.03.17.
@@ -17,48 +19,56 @@ import de.nicidienase.chaosflix.common.mediadata.entities.streaming.Room;
 
 public class EventDetailsDescriptionPresenter extends Presenter {
 
-	private final Context mContext;
+	private static final String TAG = EventDetailsDescriptionPresenter.class.getSimpleName();
+	private final Context context;
 
 	public EventDetailsDescriptionPresenter(Context context) {
-		mContext = context;
+		this.context = context;
 	}
 
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent) {
-//		DetailViewContentBinding.inflate(LayoutInflater.from(mContext));
-		View view = LayoutInflater.from(mContext).inflate(R.layout.detail_view_content, null);
-		return new ViewHolder(view);
+		DetailViewBinding binding =
+				DetailViewBinding.inflate(LayoutInflater.from(context));
+		return new DescriptionViewHolder(binding.getRoot(),binding);
 	}
 
 	@Override
 	public void onBindViewHolder(ViewHolder vh, Object item) {
+		if(! (vh instanceof DescriptionViewHolder)){
+			throw new IllegalStateException("Wrong ViewHolder");
+		}
+		DescriptionViewHolder viewHolder = (DescriptionViewHolder) vh;
+		DetailDataHolder dataHolder;
 		if (item instanceof PersistentEvent) {
 			PersistentEvent event = (PersistentEvent) item;
-
-//			titleText.setText(event.getTitle());
-//			subtitleText.setText(event.getSubtitle());
-//			String speaker = TextUtils.join(", ", event.getPersons());
-//			speakersText.setText(speaker);
-//			StringBuilder sb = new StringBuilder();
-//			sb.append(event.getDescription())
-//					.append("\n")
-//					.append("\nreleased at: ").append(event.getReleaseDate())
-//					.append("\nTags: ").append(android.text.TextUtils.join(", ", event.getTags()));
-//			descriptionText.setText(sb.toString());
-//		} else if (item instanceof Room) {
-//			Room room = (Room) item;
-//			titleText.setText(room.getDisplay());
-//			subtitleText.setText(room.getSchedulename());
+			StringBuilder sb = new StringBuilder();
+			String speaker = TextUtils.join(", ", event.getPersons());
+			sb.append(event.getDescription())
+					.append("\n")
+					.append("\nreleased at: ").append(event.getReleaseDate())
+					.append("\nTags: ").append(android.text.TextUtils.join(", ", event.getTags()));
+			dataHolder = new DetailDataHolder(event.getTitle(),
+					event.getSubtitle(),
+					speaker,
+					sb.toString());
+		} else if (item instanceof Room) {
+			Room room = (Room) item;
+			dataHolder = new DetailDataHolder(room.getDisplay(),room.getSchedulename(),"","");
+		} else {
+			Log.e(TAG,"Item is neither PersistentEvent nor Room, this should not be happening");
+			dataHolder = new DetailDataHolder("","","","");
 		}
+		viewHolder.binding.setItem(dataHolder);
 	}
 
-	public class DetailViewContent {
+	public class DetailDataHolder {
 		private String title;
 		private String subtitle;
 		private String speakers;
 		private String description;
 
-		DetailViewContent(String title, String subtitle, String speakers, String description){
+		DetailDataHolder(String title, String subtitle, String speakers, String description){
 
 			this.title = title;
 			this.subtitle = subtitle;
@@ -66,7 +76,7 @@ public class EventDetailsDescriptionPresenter extends Presenter {
 			this.description = description;
 		}
 
-		DetailViewContent(PersistentEvent event){
+		DetailDataHolder(PersistentEvent event){
 			this(event.getTitle(),event.getSubtitle(), TextUtils.join(", ", event.getPersons()), "");
 			StringBuilder sb = new StringBuilder();
 			sb.append(event.getDescription())
@@ -76,7 +86,7 @@ public class EventDetailsDescriptionPresenter extends Presenter {
 			description = sb.toString();
 		}
 
-		DetailViewContent(Room room){
+		DetailDataHolder(Room room){
 			this(room.getDisplay(),room.getSchedulename(),"","");
 		}
 
@@ -98,25 +108,20 @@ public class EventDetailsDescriptionPresenter extends Presenter {
 	}
 
 	public class DescriptionViewHolder extends ViewHolder{
-		public DescriptionViewHolder(View view) {
+
+		private final DetailViewBinding binding;
+
+		public DescriptionViewHolder(View view, DetailViewBinding binding) {
 			super(view);
+			this.binding = binding;
 		}
 
-//		private final DetailviewcontentBinding binding;
-
-//		public DescriptionViewHolder(View view, DetailviewcontentBinding binding) {
-//			super(view);
-//			this.binding = binding;
-//		}
-
-//		public DetailviewcontentBinding getBinding() {
-//			return binding;
-//		}
+		public DetailViewBinding getBinding() {
+			return binding;
+		}
 	}
 
 
 	@Override
-	public void onUnbindViewHolder(ViewHolder viewHolder) {
-
-	}
+	public void onUnbindViewHolder(ViewHolder vh) {}
 }
