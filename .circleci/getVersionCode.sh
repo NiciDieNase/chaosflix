f32013a26b9590cb8ae4559d9bbf08125b72d558
Felix <mail@nicidienase.de>
2019-05-04 23:52:03 +0200

    CircleCI: generate versionCode

---
+++ .circleci/getVersionCode.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+COUNT=$(git rev-list --count HEAD | tr -d '\n\r')
+if [ $(expr $COUNT % 2) -ne 0 ]; then
+	COUNT=$(expr $COUNT - 1)
+fi
+case $1 in
+	--touch|-t|touch) printf $COUNT ;;
+	--leanback|-l|leanback) printf $(expr $COUNT - 1) ;;
+esac
