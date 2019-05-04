22d93c61692d3142b2707f4975858a6086b2ce86
Felix <mail@nicidienase.de>
2019-05-04 12:23:40 +0200

    add AppCenter-Distribute for non-Prod builds

--- common/src/noFree/java/de/nicidienase/chaosflix/noFreeInit.kt
+++ common/src/noFree/java/de/nicidienase/chaosflix/noFreeInit.kt
@@ -1,10 +1,17 @@
 import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.AppCenterService
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
 import de.nicidienase.chaosflix.BuildConfig
 import de.nicidienase.chaosflix.ChaosflixApplication
 
 fun libsInit(application: ChaosflixApplication) {
-    AppCenter.start(application, BuildConfig.APPCENTER_ID,
-        Analytics::class.java, Crashes::class.java)
+    val modules: List<Class<out AppCenterService>>
+    if(BuildConfig.FLAVOR_stage == "prod"){
+        modules = listOf(Analytics::class.java, Crashes::class.java)
+    } else {
+        modules = listOf(Analytics::class.java, Crashes::class.java, Distribute::class.java)
+    }
+    AppCenter.start(application, BuildConfig.APPCENTER_ID, *(modules.toTypedArray()))
 }
