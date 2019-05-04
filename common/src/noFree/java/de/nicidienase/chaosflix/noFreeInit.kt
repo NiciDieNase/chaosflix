292a943a8eb913d969ccd5b28418050bd7dea617
Felix <mail@nicidienase.de>
2019-05-04 17:21:18 +0200

    Common: enable appcenter-distribution only for devNoFreeRelease

--- common/src/noFree/java/de/nicidienase/chaosflix/noFreeInit.kt
+++ common/src/noFree/java/de/nicidienase/chaosflix/noFreeInit.kt
@@ -8,10 +8,10 @@ import de.nicidienase.chaosflix.ChaosflixApplication
 
 fun libsInit(application: ChaosflixApplication) {
     val modules: List<Class<out AppCenterService>>
-    if (BuildConfig.FLAVOR_stage == "prod") {
-        modules = listOf(Analytics::class.java, Crashes::class.java)
-    } else {
+    if (BuildConfig.BUILD_TYPE == "devNoFreeRelease") {
         modules = listOf(Analytics::class.java, Crashes::class.java, Distribute::class.java)
+    } else {
+        modules = listOf(Analytics::class.java, Crashes::class.java)
     }
     AppCenter.start(application, BuildConfig.APPCENTER_ID, *(modules.toTypedArray()))
 }
