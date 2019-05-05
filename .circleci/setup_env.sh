8bd0a7e8d0dcd11a67eedd799440ba1c89c01b11
Felix <mail@nicidienase.de>
2019-05-05 13:58:32 +0200

    CircleCI: fix Mapping.txt-Upload, generated VersionNumber and name

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -7,12 +7,12 @@ else
   VERSION_NAME=$(.circleci/getVersionName.sh --dev)
 fi
 echo "export STAGE=$STAGE"
-echo "export VERSION_NAME=$VERSION_NAME"
+echo "export VERSION_NAME=\"$VERSION_NAME\""
 echo "export BUILD_TYPE_LOWER=\"$(tr '[:upper:]' '[:lower:]' <<< ${BUILD_TYPE:0:1})${BUILD_TYPE:1}\""
 echo "export STAGE_LOWER=\"$(tr '[:upper:]' '[:lower:]' <<< ${STAGE:0:1})${STAGE:1}\""
 echo "export LIBS_LOWER=\"$(tr '[:upper:]' '[:lower:]' <<< ${LIBS:0:1})${LIBS:1}\""
 
-echo "$ENCODED_KEYSTORE" | base64 --decode >> ${HOME}/code/keystore.jks
+echo "$ENCODED_KEYSTORE" | base64 --decode > ${HOME}/code/keystore.jks
 KEYSTORE=${HOME}/code/keystore.jks
 
 echo "export SIGN_CONFIG=\"-PchaosflixKeystore=$KEYSTORE\
