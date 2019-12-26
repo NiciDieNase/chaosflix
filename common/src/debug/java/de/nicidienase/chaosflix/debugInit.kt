55433cf2345708361b4f09a9013b4c9bb2a9bf0d
Felix <mail@nicidienase.de>
2019-12-26 13:36:33 +0100

    remove outdated leakCanary calls

--- common/src/debug/java/de/nicidienase/chaosflix/debugInit.kt
+++ common/src/debug/java/de/nicidienase/chaosflix/debugInit.kt
@@ -1,13 +1,7 @@
 package de.nicidienase.chaosflix
 
 import com.facebook.stetho.Stetho
-import com.squareup.leakcanary.LeakCanary
 
 fun buildTypeInit(application: ChaosflixApplication) {
     Stetho.initializeWithDefaults(application)
-    if (!LeakCanary.isInAnalyzerProcess(application)) {
-        // This process is dedicated to LeakCanary for heap analysis.
-        // You should not init your app in this process.
-        LeakCanary.install(application)
-    }
 }
