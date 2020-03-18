92c260c2640d2ee8c7c8e6c2b81e4c4f658bfb8b
Felix <mail@nicidienase.de>
2020-03-19 00:39:15 +0100

    move logic from DetailsActivity to DetailsFragment

--- touch/src/main/java/de/nicidienase/chaosflix/touch/eventdetails/EventDetailsFragment.kt
+++ touch/src/main/java/de/nicidienase/chaosflix/touch/eventdetails/EventDetailsFragment.kt
@@ -1,7 +1,11 @@
 package de.nicidienase.chaosflix.touch.eventdetails
 
+import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,9 +14,12 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,15 +27,20 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.snackbar.Snackbar
+import de.nicidienase.chaosflix.common.ChaosflixUtil
+import de.nicidienase.chaosflix.common.OfflineItemManager
 import de.nicidienase.chaosflix.common.mediadata.entities.recording.persistence.Event
+import de.nicidienase.chaosflix.common.mediadata.entities.recording.persistence.Recording
 import de.nicidienase.chaosflix.common.userdata.entities.watchlist.WatchlistItem
 import de.nicidienase.chaosflix.common.viewmodel.DetailsViewModel
 import de.nicidienase.chaosflix.common.viewmodel.ViewModelFactory
 import de.nicidienase.chaosflix.touch.R
 import de.nicidienase.chaosflix.touch.browse.adapters.EventRecyclerViewAdapter
 import de.nicidienase.chaosflix.touch.databinding.FragmentEventDetailsBinding
+import de.nicidienase.chaosflix.touch.playback.PlayerActivity
+import kotlinx.android.synthetic.main.activity_eventdetails.fragment_container
 
-class EventDetailsFragment : androidx.fragment.app.Fragment() {
+class EventDetailsFragment : Fragment() {
 
     private var appBarExpanded: Boolean = false
     private lateinit var event: Event
@@ -37,6 +49,8 @@ class EventDetailsFragment : androidx.fragment.app.Fragment() {
     private var layout: View? = null
 
     private lateinit var viewModel: DetailsViewModel
+    private var selectDialog: AlertDialog? = null
+    private var pendingDownload: Bundle? = null
 
     private lateinit var relatedEventsAdapter: EventRecyclerViewAdapter
 
@@ -100,8 +114,8 @@ class EventDetailsFragment : androidx.fragment.app.Fragment() {
             }
         })
 
-        viewModel = ViewModelProviders
-            .of(requireActivity(), ViewModelFactory.getInstance(requireContext()))
+
+        viewModel = ViewModelProvider(this, ViewModelFactory.getInstance(requireContext()))
             .get(DetailsViewModel::class.java)
 
         viewModel.setEvent(event)
@@ -120,6 +134,132 @@ class EventDetailsFragment : androidx.fragment.app.Fragment() {
                 relatedEventsAdapter.items = it
             }
         })
+
+        viewModel.state.observe(viewLifecycleOwner, Observer { liveEvent ->
+            if (liveEvent == null) {
+                return@Observer
+            }
+            val event = liveEvent.data?.getParcelable<Event>(DetailsViewModel.EVENT)
+            val recording = liveEvent.data?.getParcelable<Recording>(DetailsViewModel.RECORDING)
+            val localFile = liveEvent.data?.getString(DetailsViewModel.KEY_LOCAL_PATH)
+            val selectItems: Array<Recording>? = liveEvent.data?.getParcelableArray(DetailsViewModel.KEY_SELECT_RECORDINGS) as Array<Recording>?
+            when (liveEvent.state) {
+                DetailsViewModel.State.PlayOfflineItem -> {
+                    if (event != null && recording != null) {
+                        playItem(event, recording, localFile)
+                    }
+                }
+                DetailsViewModel.State.PlayOnlineItem -> {
+                    if (event != null && recording != null) {
+                        playItem(event, recording)
+                    }
+                }
+                DetailsViewModel.State.SelectRecording -> {
+                    if (event != null && selectItems != null) {
+                        selectRecording(event, selectItems.asList()) { e, r ->
+                            viewModel.playRecording(e, r)
+                        }
+                    }
+                }
+                DetailsViewModel.State.DownloadRecording -> {
+                    if (event != null && selectItems != null) {
+                        selectRecording(event, selectItems.asList()) { e, r ->
+                            viewModel.download(e, r).observe(viewLifecycleOwner, Observer {
+                                when (it?.state) {
+                                    OfflineItemManager.State.Downloading -> {
+                                        Snackbar.make(fragment_container, "Download started", Snackbar.LENGTH_LONG).show()
+                                    }
+                                    OfflineItemManager.State.PermissionRequired -> {
+                                        pendingDownload = liveEvent.data
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                                                    WRITE_PERMISSION_REQUEST)
+                                        }
+                                    }
+                                    OfflineItemManager.State.Done -> {}
+                                }
+                            })
+                        }
+                    }
+                }
+                DetailsViewModel.State.DisplayEvent -> {
+                    event?.let {
+                        findNavController().navigate(EventDetailsFragmentDirections.actionEventDetailsFragmentSelf(event))
+                    }
+                }
+                DetailsViewModel.State.PlayExternal -> {
+                    if (event != null) {
+                        if (selectItems != null) {
+                            selectRecording(event, selectItems.asList()) { _, r ->
+                                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(r.recordingUrl)))
+                            }
+                        } else if (recording != null) {
+                            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(recording.recordingUrl)))
+                        }
+                    }
+                }
+                DetailsViewModel.State.Error -> {
+                    Snackbar.make(binding.root, liveEvent.error ?: "An Error occured", Snackbar.LENGTH_LONG)
+                }
+            }
+        })
+    }
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == WRITE_PERMISSION_REQUEST) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                pendingDownload?.let {
+                    val recording = it.getParcelable<Recording>(DetailsViewModel.RECORDING)
+                    val event = it.getParcelable<Event>(DetailsViewModel.EVENT)
+                    if (event != null && recording != null) {
+                        viewModel.download(event, recording)
+                    }
+                }
+                pendingDownload = null
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun playItem(event: Event, recording: Recording, localFile: String? = null) {
+//        if (castService.connected) {
+//            castService.loadMediaAndPlay(recording, event)
+//        } else {
+            if (localFile != null) {
+                PlayerActivity.launch(requireContext(), event, localFile)
+            } else {
+                PlayerActivity.launch(requireContext(), event, recording)
+            }
+//        }
+    }
+
+    private fun selectRecording(event: Event, recordings: List<Recording>, action: (Event, Recording) -> Unit) {
+        if (viewModel.autoselectRecording) {
+            val optimalRecording = ChaosflixUtil.getOptimalRecording(recordings, event.originalLanguage)
+            action.invoke(event, optimalRecording)
+        } else {
+            val items: List<String> = recordings.map { getStringForRecording(it) }
+            selectRecordingFromList(items, DialogInterface.OnClickListener { _, i ->
+                action.invoke(event, recordings[i])
+            })
+        }
+    }
+
+    private fun selectRecordingFromList(items: List<String>, resultHandler: DialogInterface.OnClickListener) {
+            if (selectDialog != null) {
+                selectDialog?.dismiss()
+            }
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setItems(items.toTypedArray(), resultHandler)
+            selectDialog = builder.create()
+            selectDialog?.show()
+    }
+
+    private fun getStringForRecording(recording: Recording): String {
+        return "${if (recording.isHighQuality) "HD" else "SD"}  ${recording.folder}  [${recording.language}]"
     }
 
     private fun updateBookmark(guid: String) {
@@ -161,7 +301,7 @@ class EventDetailsFragment : androidx.fragment.app.Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                activity?.finish()
+                findNavController().popBackStack(R.id.eventsListFragment,false)
                 return true
             }
             R.id.action_play -> {
@@ -214,6 +354,7 @@ class EventDetailsFragment : androidx.fragment.app.Fragment() {
     companion object {
         private val TAG = EventDetailsFragment::class.java.simpleName
         private const val EVENT_PARAM = "event"
+        private const val WRITE_PERMISSION_REQUEST = 23
 
         fun newInstance(event: Event): EventDetailsFragment {
             val fragment = EventDetailsFragment()
