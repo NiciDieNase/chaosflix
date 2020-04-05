182f55ad2031e300548e8a9231d852ea403b8107
Felix <mail@nicidienase.de>
2020-04-05 14:48:59 +0200

    add Keep-Annotation to ProgressEventView

--- common/src/main/java/de/nicidienase/chaosflix/common/userdata/entities/progress/ProgressEventView.kt
+++ common/src/main/java/de/nicidienase/chaosflix/common/userdata/entities/progress/ProgressEventView.kt
@@ -1,9 +1,11 @@
 package de.nicidienase.chaosflix.common.userdata.entities.progress
 
+import androidx.annotation.Keep
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import de.nicidienase.chaosflix.common.mediadata.entities.recording.persistence.Event
 
+@Keep
 class ProgressEventView(
     val progress: Long,
     @ColumnInfo(name = "watch_date")
