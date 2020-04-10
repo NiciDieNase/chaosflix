c4c96e32bc4aa61546e28430f52430b8a960dc6b
Felix <mail@nicidienase.de>
2020-04-10 23:44:42 +0200

    fix broken test

--- common/src/test/java/de/nicidienase/chaosflix/common/mediadata/network/RecordingServiceTest.kt
+++ common/src/test/java/de/nicidienase/chaosflix/common/mediadata/network/RecordingServiceTest.kt
@@ -19,23 +19,23 @@ class RecordingServiceTest {
 
     @Test
     fun search() = runBlocking {
-        val searchEvents = api.searchEvents("Bahn API Chaos - jetzt international", 1)
+        val searchEvents = api.searchEvents("Bahn API Chaos - jetzt international", 1).body()!!
         assertThat(searchEvents.events.size, greaterThanOrEqualTo(1))
 
-        val searchEvents2 = api.searchEvents("Bahn API Chaos - jetzt international", 2)
+        val searchEvents2 = api.searchEvents("Bahn API Chaos - jetzt international", 2).body()!!
         assertThat(searchEvents2.events.size, equalTo(0))
     }
 
     @Test
     fun bahnApiChaos() = runBlocking {
-        val searchEvents = api.searchEvents("Bahn API Chaos", 1)
+        val searchEvents = api.searchEvents("Bahn API Chaos", 1).body()!!
         assertThat(searchEvents.events.size, greaterThanOrEqualTo(4))
     }
 
     @Test
     fun git() = runBlocking {
-        val response = api.searchEventsCall("git")
-        val response2 = api.searchEventsCall("git", 2)
+        val response = api.searchEvents("git")
+        val response2 = api.searchEvents("git", 2)
         val result = listOf(response, response2).map {
             it.headers().let {
                 it.get("total") to MediaRepository.parseLink(it.get("link") ?: "")
