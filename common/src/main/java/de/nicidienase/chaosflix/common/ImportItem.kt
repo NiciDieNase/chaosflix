6fa7f5f188de70201bafd200d3c20eca0628713e
Felix <mail@nicidienase.de>
2020-04-10 23:25:32 +0200

    fix crash on import by adding keep-annotations

--- common/src/main/java/de/nicidienase/chaosflix/common/ImportItem.kt
+++ common/src/main/java/de/nicidienase/chaosflix/common/ImportItem.kt
@@ -1,8 +1,10 @@
 package de.nicidienase.chaosflix.common
 
+import androidx.annotation.Keep
 import de.nicidienase.chaosflix.common.eventimport.FahrplanLecture
 import de.nicidienase.chaosflix.common.mediadata.entities.recording.persistence.Event
 
+@Keep
 data class ImportItem(
     val lecture: FahrplanLecture,
     var event: Event?,
