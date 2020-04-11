bb9950c85575792ae669b7e6e2cc40a4773829ab
Felix <mail@nicidienase.de>
2020-04-12 01:33:24 +0200

    add test for StreamingRepository

--- common/src/main/java/de/nicidienase/chaosflix/common/mediadata/StreamingRepository.kt
+++ common/src/main/java/de/nicidienase/chaosflix/common/mediadata/StreamingRepository.kt
@@ -3,10 +3,12 @@ package de.nicidienase.chaosflix.common.mediadata
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import de.nicidienase.chaosflix.common.AnalyticsWrapperImpl
 import de.nicidienase.chaosflix.common.mediadata.entities.streaming.LiveConference
 import de.nicidienase.chaosflix.common.mediadata.network.StreamingApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.IOException
 
 class StreamingRepository(
     private val streamingApi: StreamingApi
@@ -20,8 +22,9 @@ class StreamingRepository(
             if (response.isSuccessful) {
                 _streamingConferences.postValue(response.body())
             }
-        } catch (ex: Exception){
-            Log.e(TAG, ex.message, ex)
+        } catch (e: IOException){
+            Log.e(TAG,e.message, e)
+            AnalyticsWrapperImpl.trackException(e)
         }
         return@withContext
     }
