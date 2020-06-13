6524464fa0ce71b82bc9509dc61f1917090b9e25
Felix <mail@nicidienase.de>
2020-06-13 17:09:09 +0200

    fix streaming-url for mock flavor

--- app/src/mock/java/de/nicidienase/chaosflix/StageInit.kt
+++ app/src/mock/java/de/nicidienase/chaosflix/StageInit.kt
@@ -6,6 +6,6 @@ object StageInit : ChaosflixInitializer {
 
     override fun init(application: Application) {}
 
-    const val streamingApiBaseUrl = "https://streaming.media.ccc.de"
-    const val streamingApiPath = "/streams/v2.json"
+    const val streamingApiBaseUrl = "https://gist.githubusercontent.com"
+    const val streamingApiPath = "/NiciDieNase/1ca017f180242f0ee683a1f592efc4ed/raw/0104592b57f4b29863fd0684a510462af276f30e/example_streams_v2.json"
 }
