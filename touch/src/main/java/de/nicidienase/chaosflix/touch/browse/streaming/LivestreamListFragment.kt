7efc839710a76548975c648ba269c329ac81cd9d
Felix <mail@nicidienase.de>
2020-10-04 15:19:08 +0200

    replace MergeAdapter with ConcatAdapter (fix after updating Recyclerview-Version)

--- touch/src/main/java/de/nicidienase/chaosflix/touch/browse/streaming/LivestreamListFragment.kt
+++ touch/src/main/java/de/nicidienase/chaosflix/touch/browse/streaming/LivestreamListFragment.kt
@@ -13,9 +13,9 @@ import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.MergeAdapter
 import com.google.android.material.snackbar.Snackbar
 import de.nicidienase.chaosflix.common.mediadata.entities.streaming.StreamUrl
 import de.nicidienase.chaosflix.common.viewmodel.BrowseViewModel
@@ -65,7 +65,7 @@ class LivestreamListFragment : Fragment() {
             }
         }
 
-        binding.list.adapter = MergeAdapter(livestreamAdapter, eventInfoAdapter)
+        binding.list.adapter = ConcatAdapter(livestreamAdapter, eventInfoAdapter)
         binding.swipeRefreshLayout.setOnRefreshListener {
             updateList()
         }
