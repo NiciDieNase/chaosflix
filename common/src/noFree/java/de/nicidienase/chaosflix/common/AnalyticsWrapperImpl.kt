be45efb4a09a5b044a1240a18dfdb14b70176b39
Felix <mail@nicidienase.de>
2020-10-04 15:07:07 +0200

    Remove Appcenter-Distribution to be compliant with playstore policies

--- common/src/noFree/java/de/nicidienase/chaosflix/common/AnalyticsWrapperImpl.kt
+++ common/src/noFree/java/de/nicidienase/chaosflix/common/AnalyticsWrapperImpl.kt
@@ -6,17 +6,12 @@ import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.AppCenterService
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
-import com.microsoft.appcenter.distribute.Distribute
 import de.nicidienase.chaosflix.BuildConfig
 
 object AnalyticsWrapperImpl : AnalyticsWrapper {
     override fun init(application: Application) {
         val modules: List<Class<out AppCenterService>>
-        if (BuildConfig.BUILD_TYPE == "devNoFreeRelease") {
-            modules = listOf(Analytics::class.java, Crashes::class.java, Distribute::class.java)
-        } else {
-            modules = listOf(Analytics::class.java, Crashes::class.java)
-        }
+        modules = listOf(Analytics::class.java, Crashes::class.java)
         AppCenter.start(application, BuildConfig.APPCENTER_ID, *(modules.toTypedArray()))
     }
 
