92bf304eb0a100acd184d3fbf3ef7e3a9158a766
Felix <mail@nicidienase.de>
2020-10-09 00:51:03 +0200

    switch from requests to urllib (request is not contained in circlci docker image)

--- .circleci/amazon_upload.py
+++ .circleci/amazon_upload.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-import requests
+import urllib.request
+import urllib.parse
+import json
 
 client_id = sys.argv[1]
 client_secret = sys.argv[2]
@@ -19,10 +21,13 @@ data = {
     "scope": scope
 }
 amazon_auth_url = "https://api.amazon.com/auth/o2/token"
-auth_response = requests.post(amazon_auth_url, data=data)
+request_data = urllib.parse.urlencode(data)
+request_data = request_data.encode('ascii')
+auth_request = urllib.request.Request(url = amazon_auth_url, data=request_data)
+auth_response = urllib.request.urlopen(auth_request)
 
 # Read token from auth response
-auth_response_json = auth_response.json()
+auth_response_json = json.load(auth_response)
 auth_token = auth_response_json["access_token"]
 
 auth_token_header_value = "Bearer %s" % auth_token
@@ -33,22 +38,25 @@ auth_token_header = {"Authorization": auth_token_header_value}
 # Get Edits
 get_edits_path = '/v1/applications/%s/edits' % app_id
 get_edits_url = BASE_URL + get_edits_path
-get_edits_response = requests.get(get_edits_url, headers=auth_token_header)
-current_edit = get_edits_response.json()
+get_edits_request = urllib.request.Request(url=get_edits_url, headers=auth_token_header)
+get_edits_response = urllib.request.urlopen(get_edits_request)
+current_edit = json.load(get_edits_response)
 
 if len(current_edit) < 1:
 	# create edit
 	print("Creating new edit")
 	create_edit_path = '/v1/applications/%s/edits' % app_id
 	create_edit_url = BASE_URL + create_edit_path
-	create_edit_response = requests.post(create_edit_url, headers=auth_token_header)
-	current_edit = create_edit_response.json()
+	create_edit_request = urllib.request.Request(create_edit_url, headers=auth_token_header, method="POST")
+	create_edit_response = urllib.request.urlopen(create_edit_request)
+	current_edit = json.load(create_edit_response)
 else:
 	print("Using existing edit")
 edit_id = current_edit['id']
-
 print(edit_id)
 
+# File-Upload
+print("Uploading APK (%s)" % apk_location)
 add_apk_path = '/v1/applications/%s/edits/%s/apks/upload' % (app_id, edit_id)
 add_apk_url = BASE_URL + add_apk_path
 local_apk = open(apk_location, 'rb').read()
@@ -56,7 +64,7 @@ all_headers = {
     'Content-Type': 'application/vnd.android.package-archive'
 }
 all_headers.update(auth_token_header)
-add_apk_response = requests.post(add_apk_url, headers=all_headers, data=local_apk)
-response = add_apk_response.json()
+add_apk_request = urllib.request.Request(url=add_apk_url, headers=all_headers, data=local_apk)
+add_apk_response = urllib.request.urlopen(add_apk_request)
 
-print(response)
+print("Response: %s" % add_apk_response.status)
